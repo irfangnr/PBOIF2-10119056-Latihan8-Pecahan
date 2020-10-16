431379f10d469a0bb38441d22b571e830eced580
Irfan <>
2020-10-16 19:37:30 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan8/pecahan/PBOIF210119056Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan8.pecahan;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan pecahan
+ */
+
+public class PBOIF210119056Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f=1234567890.123456789F;
+        double d=1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
